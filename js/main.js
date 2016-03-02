7f16cd46b67448f01b7a5760bab6aca43c2b9561
Wesley Aspinall <wra427@gmail.com>
2016-03-02 03:03:58 -0500

    rendered the clock component in main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+import React from 'react';
+import ReactDom from 'react-dom';
+import Clock from './clock';
 
-console.log('Hello, World');
+
+ ReactDom.render(
+  <div>
+    <Clock format="#hhmmss"/>
+    <Clock format="hh:mm:ss"/>
+  </div>,
+  document.querySelector('.app')
+);
